bc7e4e79c0690658b6d3bf016488104eb75f416b
smferoj <sm.feroj@gmail.com>
2024-06-09 13:38:11 +0600

    first commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+ <main>
+<!-- Date -->
+
+<div>
+  {{ new Date().toLocaleDateString('en-us', {
+    weekday: 'long',
+    year:'numeric',
+    month:'long',
+    day: 'numeric'
+  }) }}
+</div>
+ </main>
+</template>
+
+<script setup>
+ 
+</script>
+
+<style>
+
+</style>
